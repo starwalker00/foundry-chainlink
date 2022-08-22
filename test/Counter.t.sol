ba6755cf2ed84bff78b333488b35e8ccae07d296
starwalker00 <aaronwalkerup@gmail.com>
2022-08-22 18:05:01 +0200

    chore: solc 0.8.15

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,14 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity 0.8.15;
 
 import "forge-std/Test.sol";
 import "../src/Counter.sol";
 
 contract CounterTest is Test {
     Counter public counter;
+
     function setUp() public {
-       counter = new Counter();
-       counter.setNumber(0);
+        counter = new Counter();
+        counter.setNumber(0);
     }
 
     function testIncrement() public {
